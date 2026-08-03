de7ee7d832f7643d900b87d8f02462036bbc62b8
Hiago Lucas <hcardealdemelo@gmail.com>
2026-08-02 23:12:44 -0300

    more solutions

--- 07-functors-applicatives-and-monoids/02-applicative-list/Solution.hs
+++ 07-functors-applicatives-and-monoids/02-applicative-list/Solution.hs
@@ -3,15 +3,16 @@ module Solution
   , allPairs
   , applyAllOps
   ) where
+import GHC.Internal.Functor.ZipList (ZipList(ZipList))
 
 -- New: Applicative for lists ([] <*> []) -- every combination of elements.
 -- See description.md for the spec of each function.
 
 allSums :: [Int] -> [Int] -> [Int]
-allSums xs ys = undefined
+allSums xs ys = (+) <$> xs <*> ys
 
 allPairs :: [a] -> [b] -> [(a, b)]
-allPairs xs ys = undefined
+allPairs xs ys = (\x y -> (x, y)) <$> xs <*> ys
 
 applyAllOps :: [Int -> Int] -> [Int] -> [Int]
-applyAllOps fs xs = undefined
+applyAllOps fs xs = fs <*> xs
